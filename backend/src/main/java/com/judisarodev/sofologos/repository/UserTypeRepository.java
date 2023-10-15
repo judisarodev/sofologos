87ea6c9d63dc00d0fa6439c5c51d7f0432389676
judisaro <judisaro@hotmail.com>
2023-10-14 21:44:15 -0500

    db set up

--- backend/src/main/java/com/judisarodev/sofologos/repository/UserTypeRepository.java
+++ backend/src/main/java/com/judisarodev/sofologos/repository/UserTypeRepository.java
@@ -3,7 +3,7 @@ package com.judisarodev.sofologos.repository;
 import com.judisarodev.sofologos.model.UserType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserTypeRepository extends JpaRepository<UserTypeRepository, Integer> {
+public interface UserTypeRepository extends JpaRepository<UserType, Integer> {
     UserType save(UserType userType);
     UserType findById(int userTypeId);
     void deleteById(int userTypeId);
